3d6768f89b8b37273af45ea122be8f092f1cf61b
Arthur Xavier <garok102@gmail.com>
2026-03-05 13:02:28 -0300

    chore

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -3,6 +3,6 @@ use yew::prelude::*;
 #[function_component()]
 pub fn Index() -> Html {
    html! {
-       <h1 class="text-lg bold">{ "this is just some text on about page" }</h1>
+       <h1 class="text-lg bold">{ "this is just some text on index page" }</h1>
    }
 }
