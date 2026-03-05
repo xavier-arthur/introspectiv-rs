5b19968fd3e23086a26fe9ff757ea26abea04b3b
Arthur Xavier <garok102@gmail.com>
2026-03-05 19:09:52 -0300

    Update gol.rs

--- src/routes/gol.rs
+++ src/routes/gol.rs
@@ -29,67 +29,65 @@ pub fn gol(props: &GolProps) -> Html {
         let canvas_ref = canvas_ref.clone();
         let grid_state_clone_effect = grid_state.clone();
         let col_state_clone_effect = col_state.clone();
-        let mounted = mounted.clone();
 
-        use_effect(move || {
-            if *mounted {
-                return;
-            }
+        use_effect_with(
+            (),
+            move |_| {
+                crate::log!("running");
 
-            crate::log!("running");
+                let canvas = canvas_ref.cast::<HtmlCanvasElement>().unwrap();
 
-            let canvas = canvas_ref.cast::<HtmlCanvasElement>().unwrap();
+                let elem: &HtmlElement = canvas.as_ref();
+                let rect = elem.get_bounding_client_rect();
 
-            let elem: &HtmlElement = canvas.as_ref();
-            let rect = elem.get_bounding_client_rect();
+                let width = rect.width();
+                let height = rect.height();
 
-            let width = rect.width();
-            let height = rect.height();
+                canvas.set_width(width as u32);
+                canvas.set_height(height as u32);
 
-            canvas.set_width(width as u32);
-            canvas.set_height(height as u32);
+                let ctx: CanvasRenderingContext2d =
+                    canvas.get_context("2d").unwrap().unwrap()
+                    .dyn_into().unwrap();
 
-            let ctx: CanvasRenderingContext2d =
-                canvas.get_context("2d").unwrap().unwrap()
-                .dyn_into().unwrap();
+                let cols = (width / cell_size).floor() as i32;
+                let rows = (height / cell_size).floor() as i32;
 
-            let cols = (width / cell_size).floor() as i32;
-            let rows = (height / cell_size).floor() as i32;
+                col_state_clone_effect.set(cols);
 
-            col_state_clone_effect.set(cols);
+                let initial_grid = vec![false; (cols * rows) as usize];
+                grid_state_clone_effect.set(initial_grid);
 
-            let initial_grid = vec![false; (cols * rows) as usize];
-            grid_state_clone_effect.set(initial_grid);
+                #[allow(deprecated)]
+                ctx.set_stroke_style_str(&color);
+                ctx.set_line_width(0.5);
+                ctx.set_fill_style_str(&color);
 
-            #[allow(deprecated)]
-            ctx.set_stroke_style_str(&color);
-            ctx.set_line_width(0.5);
-            ctx.set_fill_style_str(&color);
+                for x in 0..cols {
+                    for y in 0..rows {
+                        let index = (y * cols + x) as usize;
 
-            for x in 0..cols {
-                for y in 0..rows {
-                    let index = (y * cols + x) as usize;
-
-                    ctx.stroke_rect(
-                        x as f64 * cell_size,
-                        y as f64 * cell_size,
-                        cell_size,
-                        cell_size,
-                    );
-
-                    if grid_state_clone_effect[index] {
-                        ctx.fill_rect(
+                        ctx.stroke_rect(
                             x as f64 * cell_size,
                             y as f64 * cell_size,
                             cell_size,
-                            cell_size
+                            cell_size,
                         );
-                    }
-                }
-            };
 
-            mounted.set(true);
-        });
+                        if grid_state_clone_effect[index] {
+                            ctx.fill_rect(
+                                x as f64 * cell_size,
+                                y as f64 * cell_size,
+                                cell_size,
+                                cell_size
+                            );
+                        }
+                    }
+                };
+
+                mounted.set(true);
+            }
+        );
     }
 
     let onclick = {
@@ -102,7 +100,7 @@ pub fn gol(props: &GolProps) -> Html {
             let rect = canvas.get_bounding_client_rect();
 
             let x = e.client_x() as f64 - rect.left();
-            let y = e.client_x() as f64 - rect.top();
+            let y = e.client_y() as f64 - rect.top();
 
             let cell_x = (x / cell_size).floor() as usize;
             let cell_y = (y / cell_size).floor() as usize;
